d7f29dc1e5f1f3c3bd619e87ce37fa4a69e97c0d
jaks <jaksjuasbai@gmail.com>
2024-05-09 18:35:21 +0500

    fix

--- lib/src/feature/main/presentation/ui/main_driver_page.dart
+++ lib/src/feature/main/presentation/ui/main_driver_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:technomade/src/core/resources/resources.dart';
 import 'package:technomade/src/core/router/app_router.dart';
 import 'package:technomade/src/feature/main/presentation/widgets/main_route_card.dart';
 
